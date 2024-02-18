516ce17e58f63fa0124535be1bdab07c4b9ac21e
KarimNasreddine <abedkarimnasreddine@gmail.com>
2024-02-18 16:09:04 +0200

    Fix import

--- src/hooks/useClient.ts
+++ src/hooks/useClient.ts
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { Client } from "../../../ts-client";
+import { Client } from "../../ts-client";
 import { env } from "../env";
 
 const useClientInstance = () => {
